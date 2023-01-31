95c8e5cb2b97b1b7c5c29bf26e28121b1605ebae
Pentesting-28 <mpenav28@gmail.com>
2023-01-31 16:05:28 -0400

    Add decorators @ApiTags() @ApiOperation() users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,31 +3,39 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create user' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Show all users' })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Show user details'})
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update user data'})
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete user'})
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }

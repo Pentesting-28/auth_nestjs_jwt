37e033d973f6144f16a85de9c2b4695b9a9d62b5
Pentesting-28 <mpenav28@gmail.com>
2023-02-01 17:07:29 -0400

    Implementing 'Hash' treatment for creation and edition passwords [ newUser, updateUser ]

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,12 +6,21 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './schemas/user.schema';
 import { Model } from 'mongoose';
 
+import * as bcrypt from 'bcrypt';
+
 @Injectable()
 export class UsersService {
 
   constructor(@InjectModel(User.name) private UserModel = Model<UserDocument>){}
 
   async newUser(createUserDto: CreateUserDto) {
+
+    const { password } = createUserDto;
+
+    const passwordTreatment = await this.encryptPassword(password);
+
+    createUserDto = {...createUserDto, password: passwordTreatment};
+
     return await this.UserModel.create(createUserDto);
   }
 
@@ -24,11 +33,23 @@ export class UsersService {
   }
 
   async updateUser(id: string, updateUserDto: UpdateUserDto) {
+
+    const { password } = updateUserDto;
+
+    const passwordTreatment = await this.encryptPassword(password);
+
+    updateUserDto = {...updateUserDto, password: passwordTreatment};
+    
     await this.UserModel.findByIdAndUpdate(id, updateUserDto);
+    
     return this.UserModel.findById(id);
   }
 
   async removeUser(id: string) {
     return await this.UserModel.findByIdAndRemove(id);
+  }
+  
+  async encryptPassword(password, saltOrRounds = 10) {
+    return await bcrypt.hash(password, saltOrRounds);
   }
 }

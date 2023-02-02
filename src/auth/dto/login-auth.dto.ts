7963e53214f68bf455d51c2ae1dcd385f7c93b98
Pentesting-28 <mpenav28@gmail.com>
2023-02-02 15:05:36 -0400

    Applying validation logic for our DTO login-auth.dto.ts

--- src/auth/dto/login-auth.dto.ts
+++ src/auth/dto/login-auth.dto.ts
@@ -1,1 +1,12 @@
-export class LoginAuthDto {}
+import { IsEmail, MinLength, MaxLength } from "class-validator";
+
+export class LoginAuthDto {
+
+    @IsEmail()
+    email: string;
+
+    @MinLength(8)
+    @MaxLength(12)
+    password: string;
+    
+}
